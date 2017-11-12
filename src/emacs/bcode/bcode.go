4769e048aad162cefbee334c0804d98931f454e6
quasilyte <quasilyte@gmail.com>
2017-11-12 21:58:47 +0300

    emacs/bcode: add Func and callFrame types

--- src/emacs/bcode/bcode.go
+++ src/emacs/bcode/bcode.go
@@ -22,3 +22,33 @@ import "emacs/lisp"
 // Precise panic behavior inside byte code evaluation
 // context provided elsewhere.
 type GoFunc func(args []lisp.Object) error
+
+// Func is compiled Emacs Lisp function object.
+//
+// Properties that are not related to evaluation are
+// stored outside Func.
+//
+// Func object is safe to be executed inside multiple
+// goroutines/interpreters.
+type Func struct {
+	code   []byte
+	consts []lisp.Object
+}
+
+// callFrame holds single function call activation record data.
+// Used during function return to restore interpreter state
+// that can continue execution from the point right after the invocation.
+type callFrame struct {
+	// pc holds position inside fn code before function call
+	// that spawned this frame.
+	pc uint32
+
+	// fp holds data stack index that is used to clear
+	// stack upon function return.
+	// It is also used to store function result properly.
+	fp uint32
+
+	// fn is a function that created this frame.
+	// In other words, it is a caller function.
+	fn *Func
+}
